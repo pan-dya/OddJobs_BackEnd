0bca58d55dd26ef511265bd658a046148fa83ac2
pan-dya <pandya.limawan@gmail.com>
2023-06-05 00:21:44 +0700

    update 4

--- config/Database.js
+++ config/Database.js
@@ -1,4 +1,9 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
+import mysql2 from 'mysql2';
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
+}
+new Sequelize(options)
 
 const db = new Sequelize('wads_fp','root','',{
     host:'localhost',
