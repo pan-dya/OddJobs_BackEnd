6a0cb5a4aeebd9cbabee724f3fb95f0b12b2b720
pan-dya <pandya.limawan@gmail.com>
2023-06-05 01:13:00 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -5,7 +5,7 @@ import mysql2 from 'mysql2';
 // }
 // new Sequelize(options)
 
-const db = new Sequelize(process.env.DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+const db = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME,{
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT
